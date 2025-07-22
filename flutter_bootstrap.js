498388172ff4f658d818ca69e103c8fc7dbc98af
kirish93 <rkkirish@gmail.com>
2025-07-22 08:59:28 +0530

    Deploy Flutter web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1205277160"
+    serviceWorkerVersion: "3467569469"
   }
 });
