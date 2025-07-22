127ba3267fba8ed6f6d39dbf236d7e1174b77207
kirish93 <rkkirish@gmail.com>
2025-07-22 09:29:00 +0530

    Fix resource paths for GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "862228420"
+    serviceWorkerVersion: "2734530423"
   }
 });
